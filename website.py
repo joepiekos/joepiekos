8e1e38659efcf270b045b5d94bd2be035ca991e1
joepiekos <joe.piekos@gmail.com>
2024-01-24 23:54:11 +0000

    fixing first cricinfo scraping attempt

--- website.py
+++ website.py
@@ -11,15 +11,15 @@ app._static_folder = 'static'
 def scrape_cricinfo_news():
     url = "https://www.espncricinfo.com/cricket-news"
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         news_articles = []
 
         # Extract news articles from the HTML
-        for article in soup.find_all('div', class_='news-list'):
-            news_title = article.find('span', class_='headline').text.strip()
-            news_link = article.find('a', class_='anchor-detail')['href']
+        for article in soup.find_all('div', class_='ds-border-b ds-border-line ds-p-4'):
+            news_title = article.find('h2', class_='ds-text-title-s ds-font-bold ds-text-typo').text.strip()
+            news_link = article.find('a', class_='')['href']
             news_articles.append({'title': news_title, 'link': news_link})
 
         return news_articles
@@ -28,11 +28,11 @@ def scrape_cricinfo_news():
 
 def send_email(news_articles):
     # Set up your email configuration
-    smtp_server = 'your_smtp_server'
+    smtp_server = 'smtp.gmail.com'
     smtp_port = 587
-    smtp_username = 'your_email@example.com'
-    smtp_password = 'your_email_password'
-    recipient_email = 'recipient@example.com'
+    smtp_username = 'joe.piekos@gmail.com'
+    smtp_password = 'iduo stcc hpsb zhvc'
+    recipient_email = 'joe.piekos@gmail.com'
 
     # Create the email content
     subject = 'Daily Cricket News'
@@ -44,10 +44,20 @@ def send_email(news_articles):
     msg['To'] = recipient_email
 
     # Connect to the SMTP server and send the email
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-        server.login(smtp_username, smtp_password)
-        server.sendmail(smtp_username, recipient_email, msg.as_string())
+    #with smtplib.SMTP(smtp_server, smtp_port) as server:
+     #   server.starttls()
+      #  server.login(smtp_username, smtp_password)
+       # server.sendmail(smtp_username, recipient_email, msg.as_string())
+    
+    with smtplib.SMTP(smtp_server, smtp_port) as mail_server:
+        # identify ourselves to smtp gmail client
+        mail_server.ehlo()
+        # secure our email with tls encryption
+        mail_server.starttls()
+        # re-identify ourselves as an encrypted connection
+        mail_server.ehlo()
+        mail_server.login(smtp_username, smtp_password)
+        mail_server.sendmail(smtp_username,recipient_email,msg.as_string())
 
 @app.route('/', methods=['GET'])
 def index():
@@ -62,9 +72,11 @@ def get_daily_news():
     if news_articles:
         # Send the news articles via email
         send_email(news_articles)
-        return render_template('index.html', message='Daily news articles sent to your email!')
+        return render_template('index.html', message='Daily news articles sent to your email!', status = news_articles[0]['link'])
     else:
         return render_template('index.html', message='Failed to fetch news articles.',bethanMessage = bethanMessage)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    #tst = scrape_cricinfo_news()
+    #print(tst[0]['title'])
