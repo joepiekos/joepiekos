b85498a4245d4ce82e5a0a201d558c7552a512cb
joepiekos <joe.piekos@gmail.com>
2024-01-24 00:14:17 +0000

    change to do bethan message

--- website.py
+++ website.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    messageBethan = "hellooooo love you"
     random_number = random.randint(1, 100)  # Generates a random number between 1 and 100
-    return render_template('index.html', number=random_number)
+    return render_template('index.html', number=random_number,message = messageBethan)
 
 if __name__ == '__main__':
     app.run(debug=True)
